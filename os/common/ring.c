169ade0ed53935eb46f7148655b630e50631fac0
Gongzw <gongzw@foxmail.com>
2026-04-26 12:29:04 +0800

    add ring buffer

---
+++ os/common/ring.c
@@ -0,0 +1,81 @@
+#include "ring.h"
+#include <stdio.h>
+#include "linear_pool.h"
+
+static bool ring_push(Ring* self, const void *data);
+static bool ring_pop(Ring* self, void *data);
+
+// 析构函数声明
+static void ring_destroy(Ring* self);
+
+// TODO: 初始化数据成员
+static const RingFun ring_fun = {
+    .destroy = ring_destroy,
+	.push = ring_push,
+	.pop = ring_pop,
+};
+// 构造函数实现
+Ring* ring_create(uint32_t capacity, uint32_t size) {
+    Ring* obj = (Ring*)os_malloc(sizeof(Ring));
+    if (obj) {
+        memset(obj, 0, sizeof(Ring));
+        ring_init(obj, capacity, size);
+    }
+    return obj;
+}
+
+void ring_init(Ring* self, uint32_t capacity, uint32_t size) {
+    self->fun = &(ring_fun);
+    // TODO: 初始化数据成员
+    self->elem_size = size;
+    self->capacity = capacity;
+    self->head = 0;
+    self->tail = 0;
+    self->count = 0;
+    self->buffer = os_malloc(self->capacity * self->elem_size);
+    if (!self->buffer) {
+        os_free(self);
+        return;
+    }
+}
+
+void ring_deinit(Ring* self) {
+    // TODO: 数据成员申请资源释放
+    if (self->buffer) {
+        os_free(self->buffer);
+    }
+}
+
+// 析构函数实现
+static void ring_destroy(Ring* self) {
+    if (self != NULL) {
+        ring_deinit(self);
+        os_free(self);
+    }
+}
+
+// push method
+static bool ring_push(Ring* self, const void *data) {
+    if (self->count == self->capacity) {
+        return false;
+    }
+
+    char *byte_buffer = (char *)self->buffer;
+    memcpy(byte_buffer + self->tail * self->elem_size, data, self->elem_size);
+    self->tail = (self->tail + 1) % self->capacity;
+    self->count++;
+    return true;
+}
+// pop method
+static bool ring_pop(Ring* self, void *data) {
+    if (self->count == self->capacity) {
+        return false;
+    }
+
+    char *byte_buffer = (char *)self->buffer;
+    memcpy(data, byte_buffer + self->head * self->elem_size, self->elem_size);
+    self->head = (self->head + 1) % self->capacity;
+    self->count--;
+    return true;
+}
+
---
+++ os/common/ring.h
@@ -0,0 +1,38 @@
+#ifndef RING_H
+#define RING_H
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define GET_RING(obj) ((Ring *)obj)
+// 类声明
+typedef struct _Ring Ring;
+typedef struct _RingFun RingFun;
+// 类成员函数结构
+struct _RingFun {
+    void (*destroy)(Ring* self);
+	bool (*push)(Ring* self, const void *data);
+	bool (*pop)(Ring* self, void *data);
+
+};
+// 类结构
+struct _Ring {
+    const RingFun* fun;
+    // TODO: 添加数据成员
+    void *buffer;           /* 底层字节数组 */
+    uint32_t head;               /* 读指针（取数据位置） */
+    uint32_t tail;               /* 写指针（存数据位置） */
+    uint32_t capacity;           /* 最大元素个数 */
+    uint32_t elem_size;          /* 每个元素的字节数 */
+    uint32_t count;              /* 当前元素个数 */
+};
+
+// 构造函数声明
+Ring* ring_create(uint32_t capacity, uint32_t size);
+void ring_init(Ring* self, uint32_t capacity, uint32_t size);
+
+// 析构函数声明
+void ring_deinit(Ring* self);
+
+#endif // RING_H
